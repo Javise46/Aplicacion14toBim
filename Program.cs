ac0a2f62f5e5b4f88eafc921a5619f549999a45d
Javise46 <85516588+Javise46@users.noreply.github.com>
2021-08-09 12:40:40 -0600

    Yes
    Nada

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aplicacion14toBim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 0;
+
+            while (op!=2)
+            {
+                Console.Clear();
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("-------Cinta De Opciones-------");
+                Console.WriteLine("\n1. Crear Archivo \n2. Salir ");
+                Console.WriteLine("Digite el numero de opcion que desea");
+                Console.WriteLine("----------------------------------------------");
+                op = int.Parse(Console.ReadLine());
+              
+                Console.Clear();
+
+                if (op == 1)
+                {
+                    Console.WriteLine("Ingrese el Nombre del archivo");
+                    String archivo = Console.ReadLine() + ".txt";
+                    StreamWriter ar;
+                    ar = File.AppendText(archivo);
+                    Console.WriteLine("Escribe algo");
+                    ar.WriteLine(Console.ReadLine());
+                    ar.WriteLine(".............");
+                    ar.Close();  
+                }
+                else if (op == 2)
+                {
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
